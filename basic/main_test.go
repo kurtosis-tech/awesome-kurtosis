4ccbcf85c36ec0ee79e322b3d51397af65dcf3bc
Anders Schwartz <anders.schwartz@kurtosistech.com>
2023-01-13 16:32:00 -0500

    unit-tests: example

--- basic/main_test.go
+++ basic/main_test.go
@@ -7,13 +7,9 @@ import (
 
 /*
 This example will:
-1. Start an Ethereum network with `numParticipants` nodes
-2. Wait for all the nodes to be synced
-3. Partition the network into 2. Half of the nodes will be in partition 1, half will be in partition 2
-4. Wait for the block production to diverge in each partition
-5. Heal the partition and wait for all nodes to get back in sync
+1. xxx
 
-This test demonstrate Ethereum-forking behaviour in Kurtosis.
+This test demonstrates what a basic unit testing use case looks like using the Kurtosis Go SDK
 */
 
 const (
