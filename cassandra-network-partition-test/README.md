ffdfbe99a23a2e4887d88804aee33a857d1cc592
Gyanendra Mishra <anomaly.the@gmail.com>
2023-03-17 12:45:37 +0000

    feat: Added Golang version of Cassandra Network Partitioning Test (#66)

--- cassandra-network-partition-test/README.md
+++ cassandra-network-partition-test/README.md
@@ -1,5 +1,7 @@
 ## Cassandra Network Partition Test
 
+### Kurtosis Starlark Version
+
 This test sets up an _N_ Node Cassandra Cluster (default _N_ = 3), and then moves the last node into a separate network partition, effectively
 disconnecting the last node from the rest of the nodes in the Cassandra Cluster.
 
@@ -10,4 +12,20 @@ To run:
 
 ```
 kurtosis run github.com/kurtosis-tech/awesome-kurtosis/cassandra-network-partition-test --with-subnetworks
+```
+
+### Golang Version
+
+This test sets up an _N_ Node Cassandra Cluster (default _N_ = 3), and then moves N/2 nodes into the first partition and the remaining nodes into
+the second partition.
+
+The  test verifies that post partitioning the number of nodes marked as "UN" from the two partitions are as expected. Then it heals the partitions
+and verifies that the number of nodes seen as "UN" from both the partitions are the same and equal to the total number of nodes running.
+
+To run
+
+```
+git clone https://github.com/kurtosis-tech/awesome-kurtosis.git
+cd cassandra-network-partition-test/
+go test -timeout 5m -v
 ```
---
+++ cassandra-network-partition-test/main_test.go
@@ -0,0 +1,209 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"github.com/kurtosis-tech/kurtosis/api/golang/core/lib/enclaves"
+	"github.com/kurtosis-tech/kurtosis/api/golang/core/lib/services"
+	"github.com/kurtosis-tech/kurtosis/api/golang/engine/lib/kurtosis_context"
+	"github.com/kurtosis-tech/stacktrace"
+	"github.com/sirupsen/logrus"
+	"github.com/stretchr/testify/require"
+	"sort"
+	"strconv"
+	"strings"
+	"testing"
+)
+
+/*
+This example will:
+1. Start a Cassandra network with `numNodes` nodes
+2. Wait for all nodes to be healthy
+3. Partition the network into 2. Half of the nodes will be in partition 1, half will be in partition 2
+4. Ensure that the network is unhealthy after partition
+5. Heal the partition and wait for all nodes to get back in sync
+
+This test demonstrate Network Partitioning Testing Behavior of Kurtosis
+*/
+
+const (
+	logLevel = logrus.InfoLevel
+
+	enclaveId             = "cassandra-network-partitioning-0"
+	isPartitioningEnabled = true
+
+	cassandraStarlarkPackage = "github.com/kurtosis-tech/cassandra-package"
+
+	// number of nodes to spin up, should be greater than or equal to 3
+	numNodes = 3
+
+	firstPartition  = "subnetwork0"
+	secondPartition = "subnetwork1"
+
+	defaultParallelism = 4
+
+	noSerializedParams = ""
+	noDryRun           = false
+
+	updateServiceStarlarkTemplate = `plan.update_service(service_name = "%s", config = UpdateServiceConfig(subnetwork = "%s"))`
+	headerStarlarkTemplate        = `def run(plan):`
+)
+
+func TestCassandraNetworkPartitioning(t *testing.T) {
+
+	allowConnectionStarlark := fmt.Sprintf(`def run(plan):
+	plan.set_connection(("%s", "%s"), kurtosis.connection.ALLOWED)`, firstPartition, secondPartition)
+
+	blockConnectionStarlark := fmt.Sprintf(`def run(plan):
+	plan.set_connection(("%s", "%s"), kurtosis.connection.BLOCKED)`, firstPartition, secondPartition)
+
+	logrus.SetLevel(logLevel)
+	packageParams := fmt.Sprintf("{\"num_nodes\": %d}", numNodes)
+
+	ctx, cancelCtxFunc := context.WithCancel(context.Background())
+	defer cancelCtxFunc()
+
+	logrus.Info("------------ CONNECTING TO KURTOSIS ENGINE ---------------")
+	kurtosisCtx, err := kurtosis_context.NewKurtosisContextFromLocalEngine()
+	require.NoError(t, err, "An error occurred connecting to the Kurtosis engine")
+
+	enclaveCtx, err := kurtosisCtx.CreateEnclave(ctx, enclaveId, isPartitioningEnabled)
+	require.NoError(t, err, "An error occurred creating the enclave")
+	defer kurtosisCtx.DestroyEnclave(ctx, enclaveId)
+
+	logrus.Info("------------ EXECUTING PACKAGE ---------------")
+	starlarkRunResult, err := enclaveCtx.RunStarlarkRemotePackageBlocking(ctx, cassandraStarlarkPackage, packageParams, false, defaultParallelism)
+	require.NoError(t, err, "An error executing loading the Cassandra package")
+	require.Nil(t, starlarkRunResult.InterpretationError)
+	require.Empty(t, starlarkRunResult.ValidationErrors)
+	require.Nil(t, starlarkRunResult.ExecutionError)
+
+	cassandraNodeIds, err := getCassandraNodeIds(enclaveCtx)
+	require.NoError(t, err, "An error occurred while trying to get cassandra node ids")
+	require.Len(t, cassandraNodeIds, numNodes)
+
+	starlarkRunResult, err = updateServicesWithPartitions(ctx, enclaveCtx, cassandraNodeIds)
+	require.NoError(t, err, "An error occurred while executing Starlark to update service with partitions")
+	require.Nil(t, starlarkRunResult.InterpretationError)
+	require.Empty(t, starlarkRunResult.ValidationErrors)
+	require.Nil(t, starlarkRunResult.ExecutionError)
+
+	logrus.Info("------------ STARTING TEST CASE ---------------")
+	logrus.Info("Verifying that all nodes are up by checking both the first node and the last node")
+	cassandraNodeInFirstPartition := cassandraNodeIds[0]
+	cassandraNodeInSecondPartition := cassandraNodeIds[len(cassandraNodeIds)-1]
+
+	// wait until all  nodes are measured as UP from both partitions
+	for true {
+		upNodesMeasuredInFirstPartition, downNodesMeasuredInFirstPartition := getNumberOfUpAndDownNodes(cassandraNodeInFirstPartition, enclaveCtx, t)
+		upNodesMeasuredInSecondPartition, downNodesMeasuredInSecondPartition := getNumberOfUpAndDownNodes(cassandraNodeInSecondPartition, enclaveCtx, t)
+
+		if upNodesMeasuredInFirstPartition == upNodesMeasuredInSecondPartition && upNodesMeasuredInFirstPartition == numNodes {
+			if downNodesMeasuredInFirstPartition == downNodesMeasuredInSecondPartition && downNodesMeasuredInFirstPartition == 0 {
+				break
+			}
+		}
+
+	}
+
+	logrus.Info("------------ INDUCING PARTITION ---------------")
+	starlarkRunResult, err = enclaveCtx.RunStarlarkScriptBlocking(ctx, blockConnectionStarlark, noSerializedParams, noDryRun, defaultParallelism)
+	require.NoError(t, err, "An error occurred while executing Stalark to partition network")
+	require.Nil(t, starlarkRunResult.InterpretationError)
+	require.Empty(t, starlarkRunResult.ValidationErrors)
+	require.Nil(t, starlarkRunResult.ExecutionError)
+
+	logrus.Info("Verifying that the number of up and down nodes in different partitions are as expected")
+	// wait until the up and down nodes are as expected post partition
+	// up nodes measured from the first partition should be numNodes/2; down nodes should be numNodes - numNodes/2
+	// up nodes measured from second partition should be numNodes - numNodes/2; down nodes should be numNodes
+	for true {
+		upNodesMeasuredInFirstPartition, downNodesMeasuredInFirstPartition := getNumberOfUpAndDownNodes(cassandraNodeInFirstPartition, enclaveCtx, t)
+		upNodesMeasuredInSecondPartition, downNodesMeasuredInSecondPartition := getNumberOfUpAndDownNodes(cassandraNodeInSecondPartition, enclaveCtx, t)
+
+		if upNodesMeasuredInFirstPartition == len(cassandraNodeIds)/2 && upNodesMeasuredInSecondPartition == len(cassandraNodeIds)-len(cassandraNodeIds)/2 {
+			if downNodesMeasuredInFirstPartition == len(cassandraNodeIds)-len(cassandraNodeIds)/2 && downNodesMeasuredInSecondPartition == len(cassandraNodeIds)/2 {
+				break
+			}
+		}
+	}
+
+	logrus.Info("------------ HEALING PARTITION ---------------")
+	starlarkRunResult, err = enclaveCtx.RunStarlarkScriptBlocking(ctx, allowConnectionStarlark, noSerializedParams, noDryRun, defaultParallelism)
+	require.NoError(t, err, "An error occurred while executing Stalark to update services to partition")
+	require.Nil(t, starlarkRunResult.InterpretationError)
+	require.Empty(t, starlarkRunResult.ValidationErrors)
+	require.Nil(t, starlarkRunResult.ExecutionError)
+
+	logrus.Info("------------ PARTITION HEALED ---------------")
+	logrus.Info("Verifying that all nodes are up by measuring both the first node and the last node")
+
+	// wait until all  nodes are measured as UP from both partitions
+	for true {
+		upNodesMeasuredInFirstPartition, downNodesMeasuredInFirstPartition := getNumberOfUpAndDownNodes(cassandraNodeInFirstPartition, enclaveCtx, t)
+		upNodesMeasuredInSecondPartition, downNodesMeasuredInSecondPartition := getNumberOfUpAndDownNodes(cassandraNodeInSecondPartition, enclaveCtx, t)
+
+		if upNodesMeasuredInFirstPartition == upNodesMeasuredInSecondPartition && upNodesMeasuredInFirstPartition == numNodes {
+			if downNodesMeasuredInFirstPartition == downNodesMeasuredInSecondPartition && downNodesMeasuredInFirstPartition == 0 {
+				break
+			}
+		}
+
+	}
+
+}
+
+func updateServicesWithPartitions(ctx context.Context, enclaveCtx *enclaves.EnclaveContext, cassandraNodeIds []services.ServiceName) (*enclaves.StarlarkRunResult, error) {
+	commands := []string{headerStarlarkTemplate}
+	for _, nodeIdForFirstPartition := range cassandraNodeIds[:len(cassandraNodeIds)/2] {
+		commands = append(commands, "\t"+fmt.Sprintf(updateServiceStarlarkTemplate, nodeIdForFirstPartition, firstPartition))
+	}
+	for _, nodeIdForSecondPartition := range cassandraNodeIds[len(cassandraNodeIds)/2:] {
+		commands = append(commands, "\t"+fmt.Sprintf(updateServiceStarlarkTemplate, nodeIdForSecondPartition, secondPartition))
+	}
+	fullStarlarkScript := strings.Join(commands, "\n")
+	return enclaveCtx.RunStarlarkScriptBlocking(ctx, fullStarlarkScript, noSerializedParams, noDryRun, defaultParallelism)
+}
+
+func getCassandraNodeIds(
+	enclaveCtx *enclaves.EnclaveContext,
+) (
+	[]services.ServiceName,
+	error,
+) {
+	servicesInEnclave, err := enclaveCtx.GetServices()
+	if err != nil {
+		return nil, stacktrace.Propagate(err, "An error occurred while getting services within the enclave")
+	}
+	var result []services.ServiceName
+	for serviceName := range servicesInEnclave {
+		result = append(result, serviceName)
+	}
+
+	sort.Slice(result, func(i, j int) bool {
+		return result[i] < result[j]
+	})
+
+	return result, nil
+}
+
+func getNumberOfUpAndDownNodes(cassandraNodeToCheck services.ServiceName, enclaveContext *enclaves.EnclaveContext, t *testing.T) (int, int) {
+	serviceContext, err := enclaveContext.GetServiceContext(string(cassandraNodeToCheck))
+	require.Nil(t, err)
+
+	code, downNodesStr, err := serviceContext.ExecCommand([]string{"/bin/sh", "-c", "nodetool status | grep DN | wc -l | tr -d '\n'"})
+	require.Nil(t, err)
+	require.Zero(t, code)
+
+	code, upNodesStr, err := serviceContext.ExecCommand([]string{"/bin/sh", "-c", "nodetool status | grep UN | wc -l | tr -d '\n'"})
+	require.Nil(t, err)
+	require.Zero(t, code)
+
+	downNodes, err := strconv.Atoi(downNodesStr)
+	require.Nil(t, err, "An error occurred converting '%v' to integer", downNodesStr)
+
+	upNodes, err := strconv.Atoi(upNodesStr)
+	require.Nil(t, err, "An error occurred converting '%v' to integer", upNodesStr)
+
+	return upNodes, downNodes
+}
