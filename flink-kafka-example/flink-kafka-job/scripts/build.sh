9fec83d1576d8d43fe74dd1aad2b008c7bd6ad65
Anders Schwartz <anders.schwartz@kurtosistech.com>
2023-04-10 09:15:58 -0400

    adjust build script

--- flink-kafka-example/flink-kafka-job/scripts/build.sh
+++ flink-kafka-example/flink-kafka-job/scripts/build.sh
@@ -5,12 +5,15 @@
 set -euo pipefail   # Bash "strict mode"
 script_dirpath="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 root_dirpath="$(dirname "${script_dirpath}")"
-build_dirpath="${root_dirpath}/lib"
+build_dirpath="${root_dirpath}/build"
+
+# Note you'll need Java (>=v11) and sbt installed before running this script
 
 # ==================================================================================================
 #                                             Constants
 # ==================================================================================================
-
+SBT_BUILD_PATH="target/scala-2.12"
+JAR_BUILD_FILE_NAME="run.jar"
 
 # =============================================================================
 #                                 Main Code
@@ -22,20 +25,20 @@ if [ ! -d "${root_dirpath}" ]; then
     exit 1
 fi
 
-cd "$root_dirpath" && sbt assembly
+cd "$root_dirpath" && sbt "clean;assembly"
 
-uber_jar_file="${root_dirpath}/target/scala-2.12/run.jar"
+uber_jar_file="${root_dirpath}/${SBT_BUILD_PATH}/${JAR_BUILD_FILE_NAME}"
 if ! [ -f "${uber_jar_file}" ]; then
-    echo "Error: No build jar (uber jar) was found at expected path: '${uber_jar_file}'" >&2
+    echo "Error: No build jar was found at expected path: '${uber_jar_file}'" >&2
     exit 1
 fi
 
 mkdir -p "${build_dirpath}"
 cp "${uber_jar_file}" "${build_dirpath}"
-build_jar_file="${build_dirpath}/run.jar"
+build_jar_file="${build_dirpath}/${JAR_BUILD_FILE_NAME}"
 if ! [ -f "${build_jar_file}" ]; then
-    echo "Error: No build jar (uber jar) was found at expected path: '${build_jar_file}'" >&2
+    echo "Error: No build jar was found after copying to: '${build_jar_file}'" >&2
     exit 1
 fi
 
-echo "Successfully copied uber jar to build directory: ${build_jar_file}"
+echo "Successfully copied jar to directory: ${build_jar_file}"
