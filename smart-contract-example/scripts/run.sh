467a21af6e82d061a313bb38f85af1f168ed11b4
Gyanendra Mishra <anomaly.the@gmail.com>
2023-04-19 22:29:52 +0100

    update script

--- smart-contract-example/scripts/run.sh
+++ smart-contract-example/scripts/run.sh
@@ -6,7 +6,12 @@ package_path="$(dirname "${script_dirpath}")"
 cd $package_path
 
 yarn
-kurtosis run github.com/kurtosis-tech/eth-network-package
+kurtosis run github.com/kurtosis-tech/eth-network-package --enclave hardhat-enclave
+
+PORT=$(kurtosis enclave inspect hardhat-enclave | grep el-client-0 | grep rpc | grep -oh "127.0.0.1\:\d*" | cut -d':' -f2)
+
+sed -i 's/<PORT>/${PORT}' hardhat.config.ts
+
 npx hardhat balances --network localnet
 
 npx hardhat compile
