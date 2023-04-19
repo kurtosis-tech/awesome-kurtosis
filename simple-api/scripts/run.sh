50678a2e8ff339135f21427fc51d93ebda878aa1
Gyanendra Mishra <anomaly.the@gmail.com>
2023-04-19 22:18:54 +0100

    bug fix

--- simple-api/scripts/run.sh
+++ simple-api/scripts/run.sh
@@ -1,7 +1,7 @@
 set -euo pipefail
 
 script_dirpath="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-package_path="$(dirname "${script_dirpath}")"
+package_path="$(dirname "${script_dirpath}")/kurtosis-package"
 
 "${script_dirpath}/build.sh"
 
