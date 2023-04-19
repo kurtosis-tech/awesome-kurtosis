351ee0499402b0c60811d7ea188c2429f2a8df63
Gyanendra Mishra <anomaly.the@gmail.com>
2023-04-19 21:19:48 +0100

    fix invocation to build script

--- simple-api/scripts/run.sh
+++ simple-api/scripts/run.sh
@@ -1,7 +1,7 @@
 script_dirpath="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 package_path="$(dirname "${script_dirpath}")"
 
-"./${script_dirpath}/build.sh"
+"${script_dirpath}/build.sh"
 
 cd "$package_path"
 
