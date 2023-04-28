f4bfeeb6c859a7871cb86511b3d6f66e9bc0c6cb
Victor Colombo <victorcolombo@users.noreply.github.com>
2023-04-28 15:45:03 +0100

    Update run.sh

--- cassandra-network-partition-test/scripts/run.sh
+++ cassandra-network-partition-test/scripts/run.sh
@@ -3,7 +3,7 @@ set -euo pipefail
 script_dirpath="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 package_path="$(dirname "${script_dirpath}")"
 
-cd "$package_path"
+cd $package_path
 kurtosis run "${package_path}/main.star" --with-subnetworks
 kurtosis clean -a
 go test -timeout 5m  -v
