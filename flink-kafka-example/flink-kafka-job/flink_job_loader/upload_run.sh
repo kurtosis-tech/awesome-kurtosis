9da0b55e016be34cdb3d97c882d4bf31f751392b
Anders Schwartz <anders.schwartz@kurtosistech.com>
2023-04-28 11:29:06 -0400

    removing

--- flink-kafka-example/flink-kafka-job/flink_job_loader/upload_run.sh
+++ flink-kafka-example/flink-kafka-job/flink_job_loader/upload_run.sh
@@ -2,7 +2,6 @@
 # 2021-07-08 WATERMARK, DO NOT REMOVE - This script was generated from the Kurtosis Bash script template
 
 set -euo pipefail   # Bash "strict mode"
-script_dirpath="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 # ==================================================================================================
 #                                             Constants
