7990e3abe9ba428de5ea2e17b5e922e05baba0cb
Anders Schwartz <anders.schwartz@kurtosistech.com>
2023-04-28 12:43:45 -0400

    feat: multi arch build script

--- flink-kafka-example/flink-kafka-job/docker_build_flink_job_loader.sh
+++ flink-kafka-example/flink-kafka-job/docker_build_flink_job_loader.sh
@@ -11,12 +11,47 @@ IMAGE="kurtosistech/flink-kafka-example-job-loader"
 APPLICATION_NAME="flink_job_loader"
 
 # ==================================================================================================
-#                                       Arg Parsing & Validation
-# ==================================================================================================
-
-# ==================================================================================================
 #                                             Main Logic
 # ==================================================================================================
 bash "${script_dirpath}/scripts/build.sh"
 
-docker build -f "${script_dirpath}/${APPLICATION_NAME}/." --tag "$IMAGE" .
+buildx_platform_arg="linux/arm64,linux/amd64"
+kurtosis_docker_builder="kurtosis-docker-builder"
+docker_buildx_context='kurtosis-docker-builder-context'
+image_tags_concatenated=$IMAGE
+dockerfile_filepath="${script_dirpath}/${APPLICATION_NAME}/Dockerfile"
+dockerfile_dirpath="."
+
+if docker buildx inspect "${kurtosis_docker_builder}" &>/dev/null; then
+  echo "Removing docker buildx builder ${kurtosis_docker_builder} as it seems to already exist"
+  if ! docker buildx rm ${kurtosis_docker_builder} &>/dev/null; then
+    echo "Failed removing docker buildx builder ${kurtosis_docker_builder}. Try removing it manually with 'docker buildx rm ${kurtosis_docker_builder}' before re-running this script"
+    exit 1
+  fi
+fi
+if docker context inspect "${docker_buildx_context}" &>/dev/null; then
+  echo "Removing docker context ${docker_buildx_context} as it seems to already exist"
+  if ! docker context rm ${docker_buildx_context} &>/dev/null; then
+    echo "Failed removing docker context ${docker_buildx_context}. Try removing it manually with 'docker context rm ${docker_buildx_context}' before re-running this script"
+    exit 1
+  fi
+fi
+
+## Create Docker context and buildx builder
+if ! docker context create "${docker_buildx_context}" &>/dev/null; then
+  echo "Error: Docker context creation for buildx failed" >&2
+  exit 1
+fi
+if ! docker buildx create --use --name "${kurtosis_docker_builder}" "${docker_buildx_context}" &>/dev/null; then
+  echo "Error: Docker context switch for buildx failed" >&2d
+  exit 1
+fi
+
+## Actually build the Docker image
+docker_buildx_cmd="docker buildx build --push --platform ${buildx_platform_arg} -t ${image_tags_concatenated} -f ${dockerfile_filepath} ${dockerfile_dirpath}"
+echo "Running the following docker buildx command:"
+echo "${docker_buildx_cmd}"
+if ! eval "${docker_buildx_cmd}"; then
+  echo "Error: Docker build failed" >&2
+  exit 1
+fi
