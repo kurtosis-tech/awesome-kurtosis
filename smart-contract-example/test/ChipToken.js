44b04d59d11dc0232cec7fdb15c04d9dd55388ba
Gyanendra Mishra <anomaly.the@gmail.com>
2023-11-03 13:41:31 +0000

    work with chiptoken

--- smart-contract-example/test/ChipToken.js
+++ smart-contract-example/test/ChipToken.js
@@ -3,12 +3,13 @@ const { ethers } = require('hardhat');
 require("@nomicfoundation/hardhat-chai-matchers");
 
 
-const PLAYER_ONE = '0x8943545177806ED17B9F23F0a21ee5948eCaa776'
+const PLAYER_ONE = '0x878705ba3f8bc32fcf7f4caa1a35e72af65cf766'
 
 describe('ChipToken', function () {
     beforeEach(async function () {
         this.ChipToken = await ethers.getContractFactory("ChipToken");
         this.chips = await this.ChipToken.deploy();
+        await this.chips.deployed();
     });
 
     // Network needs to bootstrap before running this test successfully needs (~1 min)
@@ -16,8 +17,18 @@ describe('ChipToken', function () {
         it('should mint 1000 chips for PLAYER ONE', async function () {
             await this.chips.mint(PLAYER_ONE, 1000);
 
+            // We wait 100 seconds for the genesis delay
+            // default slot time is 12 seconds, so we wait 13 in case
+            // Need to wait a slot for this transaction to be posted in the next block
+            // TODO: Implement a way to mine blocks in eth-network-package instantly to eliminate needing to wait (eg. hardhat 'evm_mine')
+            await sleep(13000+100000);
+
             const playerOneBalance = await this.chips.balanceOf(PLAYER_ONE)
             expect(playerOneBalance).to.equal(1_000);
         }).timeout(1000000);
     });
 });
+
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
