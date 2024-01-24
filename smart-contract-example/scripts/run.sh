372568e4d0dd27d285475ac03767e40bb4cbf92d
Gyanendra Mishra <anomaly.the@gmail.com>
2024-01-24 09:20:26 +0000

    fix genesis delay

--- smart-contract-example/scripts/run.sh
+++ smart-contract-example/scripts/run.sh
@@ -6,7 +6,7 @@ package_path="$(dirname "${script_dirpath}")"
 cd $package_path
 
 yarn
-kurtosis run github.com/kurtosis-tech/ethereum-package  --enclave hardhat-enclave '{"network_params": {"capella_fork_epoch": 10}}'
+kurtosis run github.com/kurtosis-tech/ethereum-package  --enclave hardhat-enclave '{"network_params": {"capella_fork_epoch": 10, "genesis_delay": 120}}'
 
 PORT=$(kurtosis enclave inspect hardhat-enclave | grep "rpc: 8545/tcp" | grep -oh "127.0.0.1\:[0-9]*" | cut -d':' -f2)
 
