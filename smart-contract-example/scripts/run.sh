6e8812702724f4342389a53923f7f911ccbf188b
Gyanendra Mishra <anomaly.the@gmail.com>
2024-03-11 14:58:56 +0000

    pin an older version

--- smart-contract-example/scripts/run.sh
+++ smart-contract-example/scripts/run.sh
@@ -6,12 +6,12 @@ package_path="$(dirname "${script_dirpath}")"
 cd $package_path
 
 yarn
-kurtosis run github.com/kurtosis-tech/ethereum-package  --enclave hardhat-enclave --args-file ./args.yaml
+kurtosis run github.com/kurtosis-tech/ethereum-package@1.4.0  --enclave hardhat-enclave --args-file ./args.yaml
 
 PORT=$(kurtosis enclave inspect hardhat-enclave | grep "rpc: 8545/tcp" | grep -oh "127.0.0.1\:[0-9]*" | cut -d':' -f2)
 
 # sed -i '' "s/<PORT>/$PORT/" hardhat.config.ts if you want to run it on Mac
-sed -i "s/<PORT>/$PORT/" hardhat.config.ts
+gsed -i "s/<PORT>/$PORT/" hardhat.config.ts
 
 npx hardhat balances --network localnet
 
