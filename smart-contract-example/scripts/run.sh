bd8ba4f21766f422e5820a93f24a684691fb18ec
Gyanendra Mishra <anomaly.the@gmail.com>
2024-07-17 17:09:16 +0100

    Update run.sh

--- smart-contract-example/scripts/run.sh
+++ smart-contract-example/scripts/run.sh
@@ -6,7 +6,7 @@ package_path="$(dirname "${script_dirpath}")"
 cd $package_path
 
 yarn
-kurtosis run github.com/kurtosis-tech/ethereum-package  --enclave hardhat-enclave --args-file ./args.yaml
+kurtosis run github.com/ethpandaops/ethereum-package  --enclave hardhat-enclave --args-file ./args.yaml
 
 PORT=$(kurtosis enclave inspect hardhat-enclave | grep "rpc: 8545/tcp" | grep -oh "127.0.0.1\:[0-9]*" | cut -d':' -f2)
 
