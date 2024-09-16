41a6d0b5ad28a6a9068d69de6fb28a9678bd443b
Laurent Luce <laurentluce49@yahoo.com>
2024-09-16 12:17:12 -0400

    Update to work against latest readyset package

--- readyset-example/scripts/run.sh
+++ readyset-example/scripts/run.sh
@@ -1,8 +1,5 @@
 set -euo pipefail
 
-# Bypass until the readyset package is updated to work with the latest kurtosis
-exit 0
-
 script_dirpath="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 package_path="$(dirname "${script_dirpath}")"
 
